6568678e70668aa07e7faa2caa29697c6d995286
Ethan <ethan.quach.10@gmail.com>
2016-09-22 16:58:41 -0700

    A BIB A TOOT

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/inandoutburger/Order.java
@@ -0,0 +1,14 @@
+package edu.orangecoastcollege.cs273.inandoutburger;
+
+public class Order {
+    private int mHamburgers, mCheeseburgers, mDoubleDoubles, mFrenchFries,
+            mShakes, mLargeDrinks, mMediumDrinks, mSmallDrinks;
+    final private double PRICE_HAMBURGER = 2.15, PRICE_CHEESEBURGER = 2.45,
+            PRICE_DOUBLE_DOUBLE = 3.60, PRICE_FRENCH_FRIES = 1.65,
+            PRICE_SHAKE = 2.20, PRICE_LARGE_DRINK = 1.75,
+            PRICE_MEDIUM_DRINK = 1.55, PRICE_SMALL_DRINK = 1.45;
+
+    public Order(){
+
+    }
+}
